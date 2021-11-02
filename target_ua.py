e25822037c903c21411866037e5edafbfe3e6246
codefloww <kryvpaul@gmail.com>
2021-11-02 13:25:42 +0200

    fixed to sutisfy rules

--- target_ua.py
+++ target_ua.py
@@ -4,11 +4,16 @@ import random
 
 def generate_grid():
     """Generates list of lists of letters - i.e. grid for the game.
-    e.g. [['ж', 'л, 'р'], ['с', 'о', 'к'], ['р', 'т', 'і']]
+    e.g. ['ж', 'л, 'р','в','о']
 
     Returns:
-        list[list[str]]: playing grid of letters
+        list[str]: playing grid of letters
         str: part of language to guess words from
+
+    >>> len(generate_grid())
+    5
+    >>> type(generate_grid())
+    <class 'list'>
     """
     alphabet = [
         "а",
@@ -45,21 +50,16 @@ def generate_grid():
         "ю",
         "я",
     ]
-    letters_grid = [
-        [alphabet[random.randint(0, 32)] for _ in range(3)] for _ in range(3)
-    ]
+    letters_grid = [alphabet[random.randint(0, 32)] for _ in range(5)]
     # check whether 5 different letters appear in grid
     unique = ""
-    for row in range(3):
-        for col in range(3):
-            if letters_grid[row][col] not in unique:
-                unique += letters_grid[row][col]
+    for letter in range(5):
+        if letters_grid[letter] not in unique:
+            unique += letters_grid[letter]
     if len(unique) < 5:
         letters_grid = generate_grid()
 
-    part = random.choice(["noun", "verb", "adjective", "adverb"])
-
-    return letters_grid, part
+    return letters_grid
 
 
 def display_grid(letters_grid, part) -> None:
@@ -85,6 +85,19 @@ def get_words(file: str, letters):
 
     Returns:
         list[tuple(str,str)]: list of words that are with rules
+
+    >>> get_words('base.lst', ['є'])
+    [('євнух', 'noun'), ('єврей', 'noun'), ('євро', 'noun'), ('єгер', 'noun'), ('єдваб', 'noun'), \
+('єзуїт', 'noun'), ('єлей', 'noun'), ('ємний', 'adjective'), ('ємно', 'adverb'), \
+('єна', 'noun'), ('єнот', 'noun'), ('єпарх', 'noun'), ('єресь', 'noun'), ('єри', 'noun'), \
+('єрик', 'noun'), ('єрик', 'noun'), ('єство', 'noun'), ('єті', 'noun'), ('єхида', 'noun')]
+    >>> get_words('base.lst', ['ь','ґ'])
+    [('ґаблі', 'noun'), ('ґава', 'noun'), ('ґавин', 'adjective'), ('ґазда', 'noun'), \
+('ґалій', 'noun'), ('ґандж', 'noun'), ('ґандж', 'noun'), ('ґанок', 'noun'), ('ґара', 'noun'), \
+('ґвалт', 'noun'), ('ґевал', 'noun'), ('ґедз', 'noun'), ('ґедзь', 'noun'), ('ґзимс', 'noun'), \
+('ґлей', 'noun'), ('ґміна', 'noun'), ('ґніт', 'noun'), ('ґо', 'noun'), ('ґолда', 'noun'), \
+('ґонт', 'noun'), ('ґонта', 'noun'), ('ґрата', 'noun'), ('ґрати', 'noun'), ('ґрис', 'noun'), \
+('ґроно', 'noun'), ('ґрунт', 'noun'), ('ґуґля', 'noun'), ('ґудзь', 'noun'), ('ґуля', 'noun')]
     """
     legal_words = []
     with open(file, "r") as dictionary:
@@ -99,34 +112,18 @@ def get_words(file: str, letters):
                 part_lang = "adjective"
             elif "adv" in part_lang:
                 part_lang = "adverb"
+            else:
+                part_lang = ""
             if (
                 check_rules(letters, word[:stop])
                 and word[:stop].lower() not in legal_words
+                and part_lang != ""
             ):
                 legal_words.append((word[:stop].lower(), part_lang))
     for word in legal_words:
         if word[0] == "":
             legal_words.remove(word)
     return legal_words
-
-
-def count_appearances(word: str):
-    """Checks how many appearance of each letter in word
-
-    Args:
-        word (str): word to check
-
-    Returns:
-        list[tuple]: list of tuples('letter', appearances)
-    """
-    counter_letter = []
-    word = word.lower()
-    for i in range(len(word)):
-        if i == 0:
-            counter_letter.append((word[i], word.count(word[i])))
-        elif word[i] not in word[:i] and i != 0:
-            counter_letter.append((word[i], word.count(word[i])))
-    return counter_letter
 
 
 def check_rules(letters, word) -> bool:
@@ -140,15 +137,9 @@ def check_rules(letters, word) -> bool:
         bool: correctness of wor according to rules
     """
     correct = False
-    if len(word) <= 5 and letters[4] in word.lower():
-        correct = True
-        count_word = count_appearances(word.lower())
-        for appearance in count_word:
-            if appearance[0] not in letters or appearance[1] > letters.count(
-                appearance[0]
-            ):
-                correct = False
-                break
+    for letter in letters:
+        if len(word) <= 5 and word[0].lower() == letter:
+            correct = True
     return correct
 
 
@@ -163,6 +154,15 @@ def check_user_words(user_words, language_part: str, letters, dict_of_words):
 
     Returns:
         list: [description]
+
+    >>> check_user_words([], "adverb", ['ш', 'ь', 'т', 'і', 'х'], get_words("base.lst", ['ш', 'ь', 'т', 'і', 'х']))  # pylint: disable=line-too-long
+    ([], ['ізнов', 'інак', 'інако', 'інде', 'іноді', 'іще', 'тамки', 'темно', 'тепер', 'тепло', \
+'тихо', 'тихше', 'тоді', 'торік', 'точно', 'тричі', 'трохи', 'туго', 'туди', 'тудою', 'тужно', \
+'тут', 'тутки', 'тюпки', 'тяжко', 'хапко', 'хибко', 'хижо', 'хирно', 'хитро', 'хмуро', 'хором', \
+'худо', 'хутко', 'шумко', 'шумно'])
+    >>> check_user_words(['гаяти', 'гнати', 'ініціалізація', 'узяти', 'щавель'], "verb", ['ю', 'щ', 'я', 'ц', 'г'], get_words("base.lst", ['ю', 'щ', 'я', 'ц', 'г']))  # pylint: disable=line-too-long
+    (['гаяти', 'гнати'], ['гнити', 'гнути', 'гоїти', 'грати', 'гріти', 'густи', \
+'юшити', 'явити', 'яріти', 'ячати'])
     """
     if letters == letters:
         why = 0
@@ -170,11 +170,11 @@ def check_user_words(user_words, language_part: str, letters, dict_of_words):
     why -= 1
     correct_user_words = []
     missed_words = []
-    for dict in dict_of_words:
-        if dict[0] not in user_words and dict[1] == language_part:
-            missed_words.append(dict[0])
-        elif dict[0] in user_words and dict[1] == language_part:
-            correct_user_words.append(dict[0])
+    for word in dict_of_words:
+        if word[0] not in user_words and word[1] == language_part:
+            missed_words.append(word[0])
+        elif word[0] in user_words and word[1] == language_part:
+            correct_user_words.append(word[0])
     return correct_user_words, missed_words
 
 
@@ -193,7 +193,7 @@ def get_user_words():
     return user_words[:-1]
 
 
-def results(correct_words, missed_words, file: str):
+def results(correct_words, missed_words: str):
     """prints result of game to user and saves result in results.txt
 
     Args:
@@ -213,7 +213,8 @@ def results(correct_words, missed_words, file: str):
 
 def main() -> None:
     """maintains a full process of game"""
-    grid, language_part = generate_grid()
+    grid = generate_grid()
+    language_part = random.choice(["noun", "verb", "adjective", "adverb"])
     display_grid(grid, language_part)
     letters = [grid[i][j] for i in range(len(grid)) for j in range(len(grid[i]))]
     legal_words = get_words("base.lst", letters)
@@ -221,8 +222,11 @@ def main() -> None:
     correct_words, missed_words = check_user_words(
         user_words, language_part, letters, legal_words
     )
-    results(correct_words, missed_words, "results.txt")
+    results(correct_words, missed_words)
 
 
 if __name__ == "__main__":
+    import doctest
+
+    print(doctest.testmod())
     main()
